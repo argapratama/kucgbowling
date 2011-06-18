48d4c2567b79ad5ac42348bb2775f565cf10ef55
sic_1234 <sic_1234@naver.com>
2011-06-18 17:38:00 +0000

    레이 트레이싱 추가

---
+++ Term/Bowling/trunk/Source/LethalBowling/RayTraceKd.h
@@ -0,0 +1,5 @@
+//LNK2005: "private: static class std::locale::_Locimp * __cdecl std::locale::_Getgloballocale(void)" (?_Getgloballocale@locale@std@@CAPAV_Locimp@12@XZ)이(가) msvcprtd.lib(MSVCP100D.dll)에 이미 정의되어 있습니다.	C:\Documents and Settings\고려대학교\바탕 화면\opengl\kucgbowling\trunk\Term\Bowling\trunk\Source2\LethalBowling\libcpmtd.lib
+//msvcprtd.lib;MSVCRTD.lib
+
+//1>LINK : warning LNK4098: 'msvcrtd.lib' defaultlib가 다른 라이브러리와 충돌합니다. /NODEFAULTLIB:library를 사용하십시오.
+//1>LINK : warning LNK4098: 'LIBCMTD' defaultlib가 다른 라이브러리와 충돌합니다. /NODEFAULTLIB:library를 사용하십시오.
